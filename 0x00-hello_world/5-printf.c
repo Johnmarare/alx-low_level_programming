2038f435a66ff07063f1745bac1191b704cbbe1f
Johnmarare <mararejohn@gmail.com>
2023-03-16 15:48:22 +0000

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point.
+ *
+ * Description: learning code showing the use of the printf() function
+ *
+ * Return: Always returns 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
