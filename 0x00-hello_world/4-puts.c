75ebcfb17c38b4fc5fb026cd4afab42b4ab7d40d
Johnmarare <mararejohn@gmail.com>
2023-03-16 15:37:16 +0000

    betty betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'The program is for lerning the function put()'
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
