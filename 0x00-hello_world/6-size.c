cfa316ec43f62bd0b9697978340f022a34c11644
Johnmarare <mararejohn@gmail.com>
2023-03-17 06:23:20 +0000

    trial 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,6 @@ int main(void)
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %li byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
+
 	return (0);
 }
