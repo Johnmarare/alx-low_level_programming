185c0c7fb9b7e39a5de08bfc5b8659afc9d2701c
Johnmarare <mararejohn@gmail.com>
2023-03-18 11:31:57 +0000

    print single numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: always returns 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+
+		printf("%d\n", n);
+
+
+	return (0);
+}
