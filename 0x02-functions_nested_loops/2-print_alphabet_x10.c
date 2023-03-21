899d72eabc2f649f8840a9d7df4c0d53a5ee8f9d
Johnmarare <mararejohn@gmail.com>
2023-03-21 15:45:44 +0000

    prints alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prototype function
+ * Makes alphabet x10
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		char y;
+
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
