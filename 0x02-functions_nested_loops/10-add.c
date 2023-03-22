90d5790dcb1574e419b1c1addc28d54a387d88d9
Johnmarare <mararejohn@gmail.com>
2023-03-22 04:30:27 +0000

    addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add- computes the addition of two integers
+ * @a: of int type
+ * @b: of int type
+ * Return: returns sum of @a and @b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table- Entry point
+ *
+ * @void- of type void
+ *
+ * Return: returns void.
+ */
+void times_table(void)
+
