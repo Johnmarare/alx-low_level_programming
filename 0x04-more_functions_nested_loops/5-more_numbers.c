ad74d3932e16ebac4482446cd3b4b1c607491664
Johnmarare <mararejohn@gmail.com>
2023-03-23 13:50:56 +0000

    Numbers constitute the universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point.
+ *
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+
+			_putchar((n % 10) + '0');
+
+		}	
+	_putchar('\n');
+	}
+
+}
