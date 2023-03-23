6b9e7e5cf43fada1af7bdafdcb1277101579459f
Johnmarare <mararejohn@gmail.com>
2023-03-23 20:32:04 +0000

    fix fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,21 @@ int main(void)
 	{
 		if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz\t");
 			continue;
 
 		}
-		else if ((n % 5 == 0) && (n % 3 != 0 ))
+		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz\t");
 			continue;
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz\t");
 			continue;
 		}
-		printf("%d ", n);
+		printf("%d\t", n);
 
 	}
 	printf("\n");
