6809efd0ff26bcb347c9294144feea54d628d72f
Johnmarare <mararejohn@gmail.com>
2023-03-27 19:30:42 +0000

    fix: print string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - reverses string
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int middle = length / 2;
+	char temp;
+	int i;
+
+	for (i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
