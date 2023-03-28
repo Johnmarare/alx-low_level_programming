c93674e21d21d375ec7dccc515c83c1d1e31902d
Johnmarare <mararejohn@gmail.com>
2023-03-28 16:07:12 +0000

    random password generatr

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * _atoi - convert string to integer
+ * @s: string
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; s[i] != '\0'; ++i)
+	{
+		res = res*10 + s[i] - '0';
+	}
+	return (sign*res);
+}
