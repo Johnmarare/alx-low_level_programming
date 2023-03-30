6ca24e4f0263435b7da41185a6dde89009729c5e
Johnmarare <mararejohn@gmail.com>
2023-03-30 09:37:50 +0000

    capitalize all waords of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * cap_string - converts  words of a string to caps
+ * @s: string
+ * Return: @s
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+			i++;
+		}
+		if (s[i - 1] == ' ' ||
+				s[i - 1] == '\t' ||
+				s[i - 1] == '\n' ||
+				s[i - 1] == ',' ||
+				s[i- 1] == ';' ||
+				s[i - 1] == '.' ||
+				s[i - 1] == '!' ||
+				s[i - 1] == '?' ||
+				s[i - 1] == '"' ||
+				s[i - 1] == '(' ||
+				s[i - 1] == ')' ||
+				s[i - 1] == '{' ||
+				s[i - 1] == '}' ||
+				i == 0)
+		{
+			s[i] -= 32;
+
+		
+		}
+		i++;
+	}
+	return (s);
+}
