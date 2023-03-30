e2ad26089b7afae9998c66fb82c32edb297f3e9d
Johnmarare <mararejohn@gmail.com>
2023-03-30 19:39:47 +0000

    print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - print integer
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
