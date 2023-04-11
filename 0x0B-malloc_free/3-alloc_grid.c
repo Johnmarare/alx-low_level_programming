d6e55b9270b36760f8f19c67754e3f0deac4626a
Johnmarare <mararejohn@gmail.com>
2023-04-11 17:53:43 +0000

    dynamically allocated 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - dynamically allocated 2D array
+ * @width: iterates through the rows.
+ * @height:  iterates through the columns
+ * Return: pointer to the pointer of the 2D array.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i, j;
+	int fill_value;
+
+	fill_value = 0;
+
+	matrix = malloc(sizeof(int *) * width);
+
+	for (i = 0; i < width; i++)
+	{
+		matrix[i] = malloc(sizeof(int) * height);
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			matrix[i][j] = fill_value;
+		}
+	}
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	return (matrix);
+}
+
