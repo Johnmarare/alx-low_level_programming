aa6ef4f80b95e991eac3abc4ebc2205b5f0e0a9f
Johnmarare <mararejohn@gmail.com>
2023-04-17 13:38:00 +0000

    free structs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 /**
  * free_dog - function to free memory allocated by malloc
  * @d: pointer to array
  */
 void free_dog(dog_t *d)
 {
-	int i;
-
-	for (i = 0; d[i]; i++)
-	{
-		free(i);
-	}
-	free(d[]);
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
