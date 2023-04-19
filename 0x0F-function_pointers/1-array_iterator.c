68914a311c33dbc80822cf2ad72d6771645dd695
Johnmarare <mararejohn@gmail.com>
2023-04-19 10:56:56 +0000

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - function entry point
+ * @array: array of type int
+ * @size: size of our array
+ * @action: pointer to function
+ * Return: of type void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
