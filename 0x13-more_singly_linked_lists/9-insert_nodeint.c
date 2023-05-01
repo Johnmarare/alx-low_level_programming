2f7b500de10e89ea6d2192a1a4ef9d699c5a2d53
Johnmarare <mararejohn@gmail.com>
2023-05-01 10:30:39 +0000

    inserting new node trial 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || head == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
+	new_node->next = NULL;
 
 	/*insert at beginning*/
 
@@ -33,15 +34,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx; i++)
 	{
 		/*check if idex is out of bounds*/
-		if (current == NULL)
+		if (i == idx - 1)
 		{
-			free(new_node);
-			return (NULL);
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
 		}
-		current = current->next;
+		else
+		{
+			current = current->next;
+		}
 	}
-	new_node->next = current->next;
-	current->next = new_node;
 
-	return (new_node);
+	return (NULL);
 }
