e31bec710bad41b0cc536d4dc4a35cfc25ca3950
Johnmarare <mararejohn@gmail.com>
2023-05-04 15:45:49 +0000

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * flip_bits - Finds the number of bits you would need to flip
+ *	       to get from one number to another.
+ *
+ * @n: number to convert
+ * @m: number to be converted to
+ * Return: number of bits needed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count;
+	unsigned int flip;
+
+	/*take Xor of `n` and `m` and store in flip*/
+	flip = n ^ m;
+	/*use Brian Kernighan's algorithm to count set bits*/
+	/*`count` stores the total bits set in `flip`*/
+	count = 0;
+
+	while (flip)
+	{
+		/*clear the least significant bit set*/
+		flip = flip & (flip - 1);
+		count++;
+	}
+	return (count);
+}
+
+
+
