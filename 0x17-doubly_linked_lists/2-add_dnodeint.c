9b555255f3f5829b3bdee3db3945a24c6379b954
Johnmarare <mararejohn@gmail.com>
2023-06-15 14:26:06 +0000

    add newnode at beginning of a doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * add_dnodeint - adds node at the begining of a list.
+ * @head: pointer to first node
+ * @n: data
+ * Return: new_node
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t element;
+	dlistint_t *new_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(dlistint_t));
+	if new_node == NULL
+	{
+		return (NULL);
+	}
+
+	new_node->prev = NULL;
+	new_node->next = *head;
+	new_node->n = n;
+
+	if (*head)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+
+	return (new_node);
+}
