fcfdaced08f5382e315fb454d8d2ddca0fcb6fcf
Johnmarare <mararejohn@gmail.com>
2023-12-13 07:09:03 +0000

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGO_H */
